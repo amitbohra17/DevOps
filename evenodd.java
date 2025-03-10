fbf164e247769f9bffa27340b1df6feb6ad4dee3
Amit D Jain <134262947+amitbohra17@users.noreply.github.com>
2025-03-10 09:53:29 +0530

    Create evenodd.java

---
+++ evenodd.java
@@ -0,0 +1,10 @@
+class evenodd{
+  public static void main(String[] args){
+    int a=8;
+    if(a%2==0)
+      System.out.println("Even number");
+    else
+      System.out.println9"Odd number");
+  }
+}  
+      
